be17f1515a546db1966bda84f1751a133276102f
igorfggomes <igor@cleandev.com.br>
2024-03-24 22:20:04 -0300

    refactor(product-card): extract divider component

---
+++ src/components/divider/divider.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Divider = styled.span`
+  width: 100%;
+  height: 1px;
+  background: var(--shapes);
+  margin: 8px auto;
+  padding: 0px;
+`;
--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -1,6 +1,7 @@
 import { formatPrice } from '@/utils/format-price';
 import { Card } from './styles';
 import { useRouter } from 'next/navigation';
+import { Divider } from '../divider/divider';
 
 interface ProductCardProps {
   image: string;
@@ -10,19 +11,19 @@ interface ProductCardProps {
 }
 
 export function ProductCard(props: ProductCardProps) {
-  const router = useRouter()
+  const router = useRouter();
   const price = formatPrice(props.price);
 
   const handleNavigate = () => {
-    router.push('/product?id=' + props.id)
-  }
+    router.push('/product?id=' + props.id);
+  };
 
   return (
     <Card onClick={handleNavigate}>
       <img src={props.image} />
       <div>
         <h3>{props.title}</h3>
-        <div></div>
+        <Divider />
         <p>{price}</p>
       </div>
     </Card>
